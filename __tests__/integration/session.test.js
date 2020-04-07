efc5d30835842a81f383e484b578a7c32ee7263a
wesleybruno7 <wesleybruno7@terra.com.br>
2020-04-07 17:06:24 -0300

    criando validacoes com tdd
    Criando validacoes, mas antes foram criados os testes para depois desenvolver as validacoes (dinamica TDD)

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,19 +1,87 @@
 // para executar o teste, basta digitar "yarn test" (no package.json foi criado um script "test" ao instalar o jest)
 
-const { User } = require('../../src/app/models')
+const request = require('supertest')
+
+const app = require('../../src/app')
+const truncate = require('../utils/truncate')
+const factory = require('../factories')
 
 describe("Authentication", () => {
-    it("should sum two numbes", async () => {
-        const user = await User.create({
-            name: "Wesley B. Fernandes",
-            email: "wbf@test.com.br",
-            password_hash: "123123"
+
+    beforeEach(async() => {
+        await truncate()
+    })
+
+    it('should authenticate with valid credentials', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
         })
 
-        console.log(user)
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123123'
+            })
 
-        expect(user.email).toBe('wbf@test.com.br')
+        expect(response.status).toBe(200)
 
     })
-    // it('should receive JWT token when authenticated with valid credentials', () => {})
+
+    it('should not authenticate with invalid credentials', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123456'
+            })
+
+        expect(response.status).toBe(401)
+    })
+
+    it('should return jwt token when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123123'
+            })
+
+        expect(response.body).toHaveProperty('token')
+    })
+
+    it('should be able to access private routes when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${ user.generateToken() }`)
+
+        expect(response.status).toBe(200)
+    })
+
+    it('should not be able to access private routes without jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+
+        expect(response.status).toBe(401)
+    })
+
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer 123123`)
+
+        expect(response.status).toBe(401)
+    })
 })
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const routes = require('express').Router()
-const { User } = require('./app/models')
 
-User.create({
-    name: "Wesley",
-    email: "wesley@test.com.br",
-    password_hash: "1231635161565"
+const authMiddleware = require('./app/middleware/auth')
+const SessionCotroller = require('./app/controllers/SessionController')
+
+routes.post('/sessions', SessionCotroller.store)
+
+routes.use(authMiddleware) // este middleware ira ser utilizado apenas para a rotas abaixo desta linha
+routes.get('/dashboard', (req, res) => {
+    return res.status(200).send()
 })
 
 module.exports = routes
