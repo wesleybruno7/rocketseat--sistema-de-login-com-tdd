8778882a930f62c69d692638896aea72a56112f4
wesleybruno7 <wesleybruno7@terra.com.br>
2020-04-07 15:52:53 -0300

    ambiente de teste configurado

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,14 +1,19 @@
 // para executar o teste, basta digitar "yarn test" (no package.json foi criado um script "test" ao instalar o jest)
 
+const { User } = require('../../src/app/models')
+
 describe("Authentication", () => {
-    it("should sum two numbes", () => {
-        const x = 2;
-        const y = 4;
+    it("should sum two numbes", async () => {
+        const user = await User.create({
+            name: "Wesley B. Fernandes",
+            email: "wbf@test.com.br",
+            password_hash: "123123"
+        })
 
-        const sum = x + y;
-        
-        expect(sum).toBe(6);
+        console.log(user)
 
-    });
+        expect(user.email).toBe('wbf@test.com.br')
+
+    })
     // it('should receive JWT token when authenticated with valid credentials', () => {})
-});
+})
